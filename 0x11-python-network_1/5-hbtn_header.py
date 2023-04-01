444de2eecdce923dd44b5b8baf26e87f595b096e
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-04-01 03:08:58 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    x = requests.get(sys.argv[1])
-    print(x.headers.get('X-Request-Id'))
+    req = requests.get(sys.argv[1])
+    print(req.headers.get('X-Request-Id'))
