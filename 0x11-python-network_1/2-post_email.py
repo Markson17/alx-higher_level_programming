a1a35d1531ef4723931aa7f31aa3198aaaead0ef
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-04-01 03:05:50 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
+import sys
 from urllib import request
 from urllib.parse import urlencode
-import sys
+
+
 
 if __name__ == "__main__":
-    post_dict = {"email": sys.argv[2]}
-    url_encoded_data = urlencode(post_dict)
+    post_dicts = {"email": sys.argv[2]}
+    url_encoded_data = urlencode(post_dicts)
     post_data = url_encoded_data.encode("utf-8")
     req = request.Request(sys.argv[1], post_data)
     with request.urlopen(req) as res:
