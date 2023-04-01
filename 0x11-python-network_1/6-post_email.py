a3a082a9a7f8fc1cf254f8c5705fa4271c8d1c8a
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-04-01 03:09:47 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print(r.text)
+    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(req.text)
