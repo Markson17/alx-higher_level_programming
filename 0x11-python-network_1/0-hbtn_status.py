69da5b0c093836d22f066549be01491013e20d99
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-04-01 03:01:32 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
